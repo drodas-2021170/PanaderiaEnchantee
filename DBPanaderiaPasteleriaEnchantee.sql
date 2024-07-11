eda2ec88acf9d473d96b8f00c843495a78afb188
jsican-2023091 <157516234+jsican-2023091@users.noreply.github.com>
2024-07-10 21:21:12 -0600

    Se agrega la entidad Especial a la base de datos

---
+++ DBPanaderiaPasteleriaEnchantee.sql
@@ -0,0 +1,56 @@
+drop database if exists DBPanaderiaPasteleriaEnchantee;
+create database DBPanaderiaPasteleriaEnchantee;
+
+use DBPanaderiaPasteleriaEnchantee;
+ 
+create table TipoProducto(
+	codigoTipoProducto int not null auto_increment,
+    nombreTipoProducto varchar(50),
+    descripcion varchar(100) not null,
+    temporada varchar(50) not null,
+    primary key PK_codigoTipoProducto (codigoTipoProducto)
+);
+
+create table TipoUsuario(
+	codigoTipoUsuario int auto_increment not null,
+    nombreTipoUsuario varchar(45) not null,
+    descripcion varchar(50) not null,
+    permisos varchar(50) not null,
+    estado boolean not null,
+    primary key PK_codigoTipoUsuario (codigoTipoUsuario)
+);
+
+create table Especial(
+	codigoEspecial int not null auto_increment,
+    fechaEspecial date not null,
+    cantidadPorciones varchar(30) not null,
+    masa varchar(40) not null,
+    relleno varchar(40) not null,
+    cobertura varchar(40) not null,
+    descripcionEspecial varchar(100) not null,
+    primary key PK_codigoEspecial (codigoEspecial)
+);
+
+create table Direccion(
+	codigoDireccion int not null auto_increment,
+    direccionCompleta varchar(150) not null,
+    departamento varchar(50) not null,
+    municipio varchar(50) not null,
+    otro varchar(100),
+    primary key PK_codigoDireccion (codigoDireccion) 
+);
+
+create table Locales(
+	codigoLocal int not null,
+	nombreLocal varchar(30) not null,
+    horariosLocal varchar(50) not null,
+    horariosEspecialLocal varchar(50),
+    telefonoLocal varchar(9) not null,
+    codigoDireccion int not null,
+    primary key PK_codigoLocal (codigoLocal),
+    constraint FK_Locales_Direccion foreign key
+		(codigoDireccion) references Direccion(codigoDireccion)
+);
+
+
+
