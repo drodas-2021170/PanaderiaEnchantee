36278611431e54872e9edb50fbb3f8083296700c
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2024-07-11 19:04:59 -0600

    Se agrega la estructura del proyecto conjunto con clase Conexion, Modelo Usuario y UsuarioDAO

---
+++ src/java/modelo/UsuarioDAO.java
@@ -0,0 +1,16 @@
+package modelo;
+
+import config.Conexion;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class UsuarioDAO {
+    
+    Conexion cn = new Conexion();
+    Connection con;
+    PreparedStatement ps;
+    ResultSet rs;
+    
+    
+}
