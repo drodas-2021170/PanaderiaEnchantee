eb671001af66eb70beb1621f57ab193c50d45f2c
jsican-2023091 <jsican-2023091@kinal.edu.gt>
2024-08-07 18:12:58 -0600

    Se modifica EspecialDAO, la clase Principal.jsp, Servlet Controlador y la clase Especial.jsp

--- build/web/Principal.jsp
+++ build/web/Principal.jsp
@@ -4,18 +4,87 @@
     Author     : Kendy Rodas
 --%>
 
+
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">   
-        <title>Principal</title>
+        <title>Princiapal Page</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     </head>
     <body>
-        <h1>Principal</h1>
-         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>   
+       <nav  style="background-color: #B76E79" class="navbar navbar-expand-lg navbar-light ">
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item active">
+                  <a  style="margin-left: 10px; border: none" class="btn btn-outline-light" href="Controlador?menu=Home" target="myFrame">Home <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a style="margin-left: 10px; border: none" class="btn btn-outline-light" href="Controlador?menu=TipoUsuario" target="myFrame">Tipo Usuario</a>
+              </li>
+              <li class="nav-item">
+                  <a style="margin-left: 10px; border: none" class="btn btn-outline-light" href="Controlador?menu=Usuario" target="myFrame">Usuario</a>
+              </li>
+              <li class="nav-item">
+                <a style="margin-left: 10px; border: none" class="btn btn-outline-light" href="Controlador?menu=TipoProducto&accion=Listar" target="myFrame">Tipo Producto</a>
+              </li>
+              <li class="nav-item">
+                <a style="margin-left: 10px; border: none" class="btn btn-outline-light" href="Controlador?menu=Especiales&accion=listar" target="myFrame">Especiales</a>
+              </li>
+              <li class="nav-item">
+                <a style="margin-left: 10px; border: none" class="btn btn-outline-light" href="Controlador?menu=Direccion" target="myFrame">Dirección</a>
+              </li>
+              <li class="nav-item">
+                <a style="margin-left: 10px; border: none" class="btn btn-outline-light" href="Controlador?menu=Locales" target="myFrame">Locales</a>
+              </li>
+              <li class="nav-item">
+                <a style="margin-left: 10px; border: none" class="btn btn-outline-light" href="Controlador?menu=Producto" target="myFrame">Producto</a>
+              </li>
+              <li class="nav-item">
+                <a style="margin-left: 10px; border: none" class="btn btn-outline-light" href="Controlador?menu=Promocion" target="myFrame">Promoción</a>
+              </li>
+              <li class="nav-item">
+                <a style="margin-left: 10px; border: none" class="btn btn-outline-light" href="Controlador?menu=DetalleCarrito" target="myFrame">Detalle Carrito</a>
+              </li>
+              <li class="nav-item">
+                <a style="margin-left: 10px; border: none" class="btn btn-outline-light" href="Controlador?menu=Pedido" target="myFrame">Pedido</a>
+              </li>
+              <li class="nav-item">
+                <a style="margin-left: 10px; border: none" class="btn btn-outline-light" href="Controlador?menu=Carrito" target="myFrame">Carrito</a>
+              </li>
+              <li class="nav-item">
+                <a style="margin-left: 10px; border: none" class="btn btn-outline-light" href="Controlador?menu=Factura" target="myFrame">Factura</a>
+              </li>
+            </ul>
+          </div>
+          <div class="dropdown">
+            <button style="border: none" class="btn btn-outline-light dropdown-toggle" type="button" id="dropdownButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              ${usuario.getNombresUsuario()}
+            </button>
+            <div  class="dropdown-menu text-center" aria-labelledby="dropdownButton">
+                <a class="dropdown-item" href="$">
+                    <img src="img/personaIcon.png" alt="60" width="60"/>
+                </a>
+                <a class="dropdown-item" href="#">${usuario.getUsuario()}</a>
+                <a class="dropdown-item" href="#">${usuario.getCorreoUsuario()}</a>
+                <div class="dropdown-divider"></div>
+                <form action="Validar" method="POST">
+                    <button name="accion" name="Salir" class="dropdown-item" href="#">Salir</button>
+                </form>
+            </div>
+          </div>
+       </nav>
+                <div class="m-4" style="height: 600px">
+                    <iframe name="myFrame" style="height: 100%; width: 100%; border: 1"></iframe>
+                </div>
+        
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
     </body>
 </html>
+
--- src/java/controlador/Controlador.java
+++ src/java/controlador/Controlador.java
@@ -3,23 +3,151 @@ package controlador;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Date;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import modelo.Especial;
+import modelo.EspecialDAO;
+import modelo.TipoProducto;
+import modelo.TipoProductoDAO;
 import modelo.Usuario;
 import modelo.UsuarioDAO;
 
 public class Controlador extends HttpServlet {
     Usuario usuario = new Usuario();
     UsuarioDAO usuarioDao = new UsuarioDAO();
+    TipoProducto tipoProducto = new TipoProducto();
+    TipoProductoDAO tipoProductoDao = new TipoProductoDAO();
+    int codTipoProducto;
+    Especial especial = new Especial();
+    EspecialDAO especialDao = new EspecialDAO();
+    int codEspecial;
    
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         String menu = request.getParameter("menu");
-        // String accion = request.getParameter("accion");
+        String accion = request.getParameter("accion");
         if(menu.equals("Principal")){
             request.getRequestDispatcher("Principal.jsp").forward(request, response);
+        } else if(menu.equals("Home")){
+            request.getRequestDispatcher("Home.jsp").forward(request, response);
+        } else if(menu.equals("Usuario")){
+            request.getRequestDispatcher("Usuario.jsp").forward(request, response);
+        } else if(menu.equals("TipoProducto")){
+            switch (accion) {
+                case "Listar":
+                    List listarTipoProducto = tipoProductoDao.Listar();
+                    request.setAttribute("TipoProductos", listarTipoProducto  );
+                    
+                    break;
+                case "Agregar":
+                    String nombreTipo = request.getParameter("txtNombreTipoProducto");
+                    String descripcion = request.getParameter("txtDescripcion");
+                    String temporada = request.getParameter("txtTemporada");
+                    tipoProducto.setNombreTipoProducto(nombreTipo);
+                    tipoProducto.setDescripcion(descripcion);
+                    tipoProducto.setTemporada(temporada);
+                    tipoProductoDao.Agregar(tipoProducto);
+                    request.getRequestDispatcher("Controlador?menu=TipoProducto&accion=Listar").forward(request, response);
+                    break;
+                case "Editar":
+                    codTipoProducto = Integer.parseInt(request.getParameter("codigoTipoProducto"));
+                    TipoProducto tp = tipoProductoDao.listarCodigoTipoProducto(codTipoProducto);
+                    request.setAttribute("tipoProducto", tp);
+                    request.getRequestDispatcher("Controlador?menu=TipoProducto&accion=Listar").forward(request, response);
+                    break;
+                case "Actualizar":
+                    String NombreTipo = request.getParameter("txtNombreTipoProducto");
+                    String DescripcionTipo = request.getParameter("txtDescripcion");
+                    String TemporadaTipo = request.getParameter("txtTemporada");
+                    tipoProducto.setNombreTipoProducto(NombreTipo);
+                    tipoProducto.setDescripcion(DescripcionTipo);
+                    tipoProducto.setTemporada(TemporadaTipo);
+                    tipoProducto.setCodigoTipoProducto(codTipoProducto);
+                    tipoProductoDao.Actualizar(tipoProducto);
+                    request.getRequestDispatcher("Controlador?menu=TipoProducto&accion=Listar").forward(request, response);
+                    break;
+                case "Eliminar":
+                    codTipoProducto = Integer.parseInt(request.getParameter("codigoTipoProducto"));
+                    tipoProductoDao.Eliminar(codTipoProducto);
+                    request.getRequestDispatcher("Controlador?menu=TipoProducto&accion=Listar").forward(request, response);
+                    break;
+            }
+            
+            request.getRequestDispatcher("TipoProducto.jsp").forward(request, response);
+        } else if(menu.equals("TipoUsuario")){
+            request.getRequestDispatcher("TipoUsuario.jsp").forward(request, response);
+        } else if(menu.equals("Especiales")){
+            switch(accion){
+                case "Listar":
+                    List listaEspeciales = especialDao.listar();
+                    request.setAttribute("especiales", listaEspeciales);
+                break;
+                case "Agregar":
+                    String feEspecial = request.getParameter("txtFechaEspecial");
+                    String cantiPorciones = request.getParameter("txtCantidadPorciones");
+                    String masa = request.getParameter("txtMasa");
+                    String relleno = request.getParameter("txtRelleno");
+                    String cobertura = request.getParameter("txtCobertura");
+                    String desEspecial = request.getParameter("txtDescripcionEspecial");
+                    especial.setFechaEspecial(Date.valueOf(feEspecial));
+                    especial.setCantidadPorciones(cantiPorciones);
+                    especial.setMasa(masa);
+                    especial.setRelleno(relleno);
+                    especial.setCobertura(cobertura);
+                    especial.setDescripcionEspecial(desEspecial);
+                    especialDao.agregar(especial);
+                    request.getRequestDispatcher("Controlador?menu=Especiales&accion=Listar").forward(request, response);
+                break;
+                case "Editar":
+                    codEspecial = Integer.parseInt(request.getParameter("codigoEspecial"));
+                    Especial e = especialDao.listarCodigoEspecial(codEspecial);
+                    request.setAttribute("especial", e);
+                    request.getRequestDispatcher("Controlador?menu=Especiales&accion=Listar").forward(request, response);
+                break;
+                case "Actualizar":
+                String fechaEmp = request.getParameter("txtFechaEspecial");
+                String cantidadEmp = request.getParameter("txtCantidadPorciones");
+                String masaEmp = request.getParameter("txtMasa");
+                String rellenoEmp = request.getParameter("txtRelleno");
+                String coberturaEmp = request.getParameter("txtCobertura");
+                String descripcionEmp = request.getParameter("txtDescripcionEspecial");
+                especial.setFechaEspecial(Date.valueOf(fechaEmp));
+                especial.setCantidadPorciones(cantidadEmp);
+                especial.setMasa(masaEmp);
+                especial.setRelleno(rellenoEmp);
+                especial.setCobertura(coberturaEmp);
+                especial.setDescripcionEspecial(descripcionEmp);
+                especial.setCodigoEspecial(codEspecial);
+                especialDao.actualizar(especial);
+                request.getRequestDispatcher("Controlador?menu=Especiales&accion=Listar").forward(request, response);
+                break;
+                case "Eliminar":
+                    codEspecial = Integer.parseInt(request.getParameter("codigoEspecial"));
+                    especialDao.eliminar(codEspecial);
+                    request.getRequestDispatcher("Controlador?menu=Especiales&accion=Listar").forward(request, response);
+                break;
+            }	
+            request.getRequestDispatcher("Especial.jsp").forward(request, response);
+        } else if(menu.equals("Direccion")){
+            request.getRequestDispatcher("Direccion.jsp").forward(request, response);
+        } else if(menu.equals("Locales")){
+            request.getRequestDispatcher("Locales.jsp").forward(request, response);
+        } else if(menu.equals("Producto")){
+            request.getRequestDispatcher("Producto.jsp").forward(request, response);
+        } else if(menu.equals("Promocion")){
+            request.getRequestDispatcher("Promocion.jsp").forward(request, response);
+        } else if(menu.equals("DetalleCarrito")){
+            request.getRequestDispatcher("DetalleCarrito.jsp").forward(request, response);
+        } else if(menu.equals("Pedido")){
+            request.getRequestDispatcher("Pedido.jsp").forward(request, response);
+        } else if(menu.equals("Carrito")){
+            request.getRequestDispatcher("Carrito.jsp").forward(request, response);
+        } else if(menu.equals("Factura")){
+            request.getRequestDispatcher("Factura.jsp").forward(request, response);
         }
      
     }
